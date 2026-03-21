298149d857fb0cb5a7a7d9f79dfadd9073ccd8d2
Falladyn1 <matibati300@gmail.com>
2026-03-21 16:50:51 +0100

    uzupełnienie main

--- DrzewoDecyzyjne/Program.cs
+++ DrzewoDecyzyjne/Program.cs
@@ -3,5 +3,10 @@
 ZbiorDanych baza1 = new ZbiorDanych();
 baza1.wczytajDane("iris.data");
 
-baza1.drukujDane();
+//baza1.drukujDane();
 
+Drzewo drzewo = new Drzewo();
+
+drzewo.UtworzDrzewo(baza1, 30);
+
+drzewo.WypiszDrzewo();
