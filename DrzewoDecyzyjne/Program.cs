1769274633b93062a3fcc743b75d4bbbf9b03692
Falladyn1 <matibati300@gmail.com>
2026-05-04 18:17:19 +0200

    Do obudowania

--- DrzewoDecyzyjne/Program.cs
+++ DrzewoDecyzyjne/Program.cs
@@ -3,63 +3,61 @@ using DrzewoDecyzyjne.Drzewo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 ZbiorDanych baza = new ZbiorDanych();
 baza.wczytajDane("iris.data");
 
 int k = 10; // 10-krotna walidacja
-CV cv = new CV(k, baza.LiczbaWierszy);
-List<(int[] train, int[] test)> folds = cv.makeCV();
+CV walidacjaKrzyzowa = new CV(k, baza.LiczbaWierszy);
+List<(int[] trening, int[] test)> podzbioryWalidacji = walidacjaKrzyzowa.makeCV();
 
 double sumaDokladnosci = 0;
 Console.WriteLine($"Rozpoczynam {k}-krotną walidację\n");
 
-for (int i = 0; i < folds.Count; i++)
+for (int i = 0; i < podzbioryWalidacji.Count; i++)
 {
-    var (train, test) = folds[i];
+    var (treningoweIdx, testoweIdx) = podzbioryWalidacji[i];
 
     Drzewo drzewo = new Drzewo(20);
-    drzewo.utworzDrzewo(baza, train);
+    drzewo.utworzDrzewo(baza, treningoweIdx);
 
     int poprawne = 0;
-    foreach (int testIdx in test)
+    foreach (int idxTestowy in testoweIdx)
     {
-        double[] wektorTestowy = baza.pobierzWektor(testIdx);
-        string prawdziwaEtykieta = baza.pobierzEtykiete(testIdx);
+        double[] wektorTestowy = baza.pobierzWektor(idxTestowy);
+        string prawdziwaEtykieta = baza.pobierzEtykiete(idxTestowy);
+
         if (prawdziwaEtykieta == drzewo.Test(wektorTestowy))
         {
             poprawne++;
         }
     }
 
-    double dokladnosc = (double)poprawne / test.Length * 100;
+    double dokladnosc = (double)poprawne / testoweIdx.Length * 100;
     sumaDokladnosci += dokladnosc;
-    Console.WriteLine($"Fold {i + 1}: Dokładność = {dokladnosc:F2}%");
+    Console.WriteLine($"Fałd {i + 1}: Dokładność = {dokladnosc:F2}%");
 }
 
 Console.WriteLine($"\nŚrednia skuteczność algorytmu: {sumaDokladnosci / k:F2}%");
-
-
-
-double obliczEntropie(int[] indeksy, ZbiorDanych dane)
+double ObliczEntropie(int[] indeksy, ZbiorDanych dane)
 {
     int n = indeksy.Length;
     if (n == 0) return 0.0;
 
-    var liczniki = new Dictionary<string, int>();
+    var licznikiEtykiet = new Dictionary<string, int>();
     foreach (int idx in indeksy)
     {
-        string et = dane.pobierzEtykiete(idx);
-        if (liczniki.ContainsKey(et)) liczniki[et]++;
-        else liczniki[et] = 1;
+        string etykieta = dane.pobierzEtykiete(idx);
+        if (licznikiEtykiet.ContainsKey(etykieta)) licznikiEtykiet[etykieta]++;
+        else licznikiEtykiet[etykieta] = 1;
     }
 
-    // E = - suma(pi * log2(pi))
+    // Wzór: E = - suma(p_i * log2(p_i))
     double entropia = 0.0;
-    foreach (var kvp in liczniki)
+    foreach (var para in licznikiEtykiet)
     {
-        double p = (double)kvp.Value / n;
+        double p = (double)para.Value / n;
         if (p > 0)
         {
             entropia -= p * Math.Log2(p);
@@ -72,39 +70,41 @@ double obliczEntropie(int[] indeksy, ZbiorDanych dane)
 {
     int najlepszaCecha = -1;
     double najlepszyProg = 0;
-    double najWynik = double.MaxValue;
+    double najmniejszaEntropia = double.MaxValue;
 
     for (int i = 0; i < dane.LiczbaCech; i++)
     {
         double[] progi = dane.pobierzProgi(i, indeksy);
         foreach (double prog in progi)
         {
-            List<int> lewa = new List<int>();
-            List<int> prawa = new List<int>();
+        List<int> lewaPodgrupa = new List<int>();
+        List<int> prawaPodgrupa = new List<int>();
 
-            foreach (int idx in indeksy)
-            {
-                if (dane[idx, i] <= prog) lewa.Add(idx);
-                else prawa.Add(idx);
-            }
+        foreach (int idx in indeksy)
+        {
+            if (dane[idx, i] <= prog)
+                lewaPodgrupa.Add(idx);
+            else
+                prawaPodgrupa.Add(idx);
+        }
 
-            if (lewa.Count == 0 || prawa.Count == 0) continue;
+        if (lewaPodgrupa.Count == 0 || prawaPodgrupa.Count == 0) continue;
 
-            double nl = lewa.Count;
-            double nr = prawa.Count;
-            double n = nl + nr;
+        double nLewa = lewaPodgrupa.Count;
+        double nPrawa = prawaPodgrupa.Count;
+        double nRazem = nLewa + nPrawa;
 
-            //(nl/n * El) + (nr/n * Er)
-            double wynik = (nl / n) * obliczEntropie(lewa.ToArray(), dane) +
-                           (nr / n) * obliczEntropie(prawa.ToArray(), dane);
+        // Średnia ważona entropii: (nL/n * EL) + (nR/n * ER)
+        double aktualnaEntropia = (nLewa / nRazem) * ObliczEntropie(lewaPodgrupa.ToArray(), dane) +
+                                  (nPrawa / nRazem) * ObliczEntropie(prawaPodgrupa.ToArray(), dane);
 
-            if (wynik < najWynik)
-            {
-                najWynik = wynik;
-                najlepszaCecha = i;
-                najlepszyProg = prog;
-            }
+        if (aktualnaEntropia < najmniejszaEntropia)
+        {
+            najmniejszaEntropia = aktualnaEntropia;
+            najlepszaCecha = i;
+            najlepszyProg = prog;
         }
     }
-    return (najlepszaCecha, najlepszyProg);
+}
+return (najlepszaCecha, najlepszyProg);
 }
